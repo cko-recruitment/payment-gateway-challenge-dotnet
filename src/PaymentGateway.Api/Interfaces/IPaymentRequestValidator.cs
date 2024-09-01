68b7cf1ad49ca53e38f63c5f11ade3d4242f0595
jev <jacob@jev.org.uk>
2024-09-01 10:06:55 +0100

    add payment request validator

---
+++ src/PaymentGateway.Api/Interfaces/IPaymentRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using PaymentGateway.Api.Models.Requests;
+
+namespace PaymentGateway.Api.Interfaces
+{
+    public interface IPaymentRequestValidator
+    {
+        /// <summary>
+        /// Validates a payment request
+        /// </summary>
+        /// <param name="request">The <see cref="PostPaymentRequest"/> to validate</param>
+        /// <returns>True if valid, false otherwise</returns>
+        bool Validate(PostPaymentRequest request);
+    }
+}
---
+++ src/PaymentGateway.Api/Services/PaymentRequestValidator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Frozen;
+
+using PaymentGateway.Api.Interfaces;
+using PaymentGateway.Api.Models.Requests;
+
+namespace PaymentGateway.Api.Services
+{
+    /// <inheritdoc />
+    public class PaymentRequestValidator(IIsoCodeProvider isoCodeProvider) : IPaymentRequestValidator
+    {
+        /// <inheritdoc />
+        public bool Validate(PostPaymentRequest request)
+        {
+            if (!isoCodeProvider.IsValidIsoCode(request.Currency))
+            {
+                return false;
+            }
+            return false;
+        }
+    }
+
+
+    public interface IIsoCodeProvider
+    {
+        public bool IsValidIsoCode(string isoCode);
+    }
+
+
+    public class IsoCodeProvider : IIsoCodeProvider
+    {
+        private static readonly FrozenSet<string> _allowedIsoList;
+
+        static IsoCodeProvider()
+        {
+            _allowedIsoList = new[] {
+                "usd",
+                "gbp",
+                "eur"}.ToFrozenSet();
+        }
+
+        public bool IsValidIsoCode(string isoCode)
+        {
+            return _allowedIsoList.Contains(isoCode, StringComparer.InvariantCultureIgnoreCase);
+        }
+    }
+}
