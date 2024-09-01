0cc9b8ad4fe9a62b290ea8ba76ac071054c00b85
jev <jacob@jev.org.uk>
2024-09-01 10:06:40 +0100

    add payments handler

---
+++ src/PaymentGateway.Api/Interfaces/IPaymentsHandler.cs
@@ -0,0 +1,20 @@
+﻿using PaymentGateway.Api.Models.Requests;
+using PaymentGateway.Api.Models.Responses;
+
+namespace PaymentGateway.Api.Interfaces
+{
+    public interface IPaymentsHandler
+    {
+        /// <summary>
+        /// Handles a GetPaymentRequest
+        /// </summary>
+        /// <param name="paymentId">The payment id</param>
+        Task<PostPaymentResponse> HandleGetPaymentAsync(Guid paymentId);
+
+        /// <summary>
+        /// Handles a PostPaymentRequest
+        /// </summary>
+        /// <param name="paymentId">The payment id</param>
+        Task<PostPaymentResponse> HandlePostPaymentAsync(PostPaymentRequest paymentRequest);
+    }
+}
---
+++ src/PaymentGateway.Api/Services/PaymentsHandler.cs
@@ -0,0 +1,34 @@
+﻿using PaymentGateway.Api.Interfaces;
+using PaymentGateway.Api.Models.Requests;
+using PaymentGateway.Api.Models.Responses;
+
+namespace PaymentGateway.Api.Services
+{
+    /// <inheritdoc />
+    public class PaymentsHandler(
+        IPaymentsRepository paymentsRepository,
+        IPaymentRequestValidator paymentRequestValidator
+        
+      ) : IPaymentsHandler
+    {
+        /// <inheritdoc />
+        public Task<PostPaymentResponse> HandleGetPaymentAsync(Guid id)
+        {
+            return Task.FromResult(paymentsRepository.Get(id));
+        }
+
+        /// <inheritdoc />
+        public Task<PostPaymentResponse> HandlePostPaymentAsync(PostPaymentRequest paymentRequest)
+        {
+            bool valid = paymentRequestValidator.Validate(paymentRequest);
+            if (valid)
+            {
+
+            }
+            return Task.FromResult(new PostPaymentResponse());
+        }
+
+
+
+    }
+}
